352842eddb1d16f48710c614353f7e35aedee60a
frankbenevento <fb.mentoslabs@gmail.com>
2018-09-11 05:31:31 -0700

    i have the form submitting and data is rendering.

--- index.js
+++ index.js
@@ -1,61 +1,86 @@
-const Form = (props) =>{
+class Form extends React.Component {
+    state = {
+      companyName:''
+  };
+    handleSubmit = (event) => {
+      event.preventDefault();
+    this.props.onSubmit({CompanyName: this.state.companyName});
+    this.state.companyName = ''
+  };
+  render(){
     return(
-      <form className="frmAddJobApplication">
-          <input type='text' placeholder='Company Name' />
-          <input type='text' placeholder='Date Contacted' />
+      <form onSubmit={this.handleSubmit} className="frmAddJobApplication">
+          <input
+          value = {this.state.companyName}
+          onChange = {(event)=> this.setState({ companyName: event.target.value})}
+          type='text' placeholder='Company Name' />
+          <input
+          ref={(input) => this.DateContacted = input}
+          type='text' placeholder='Date Contacted' />
           <button className='btnSubmit' type='submit'> Add Company </button>
       </form>
       )
-  }
-        const Job = (props) => {
-    return(
-        <div>
-            {props.CompanyName} 
-        </div>
+  }            
+}
+      const Job = (props) => {
+  return(
+      <div>
+          {props.CompanyName} 
+      </div>
+  )
+}
+const JobList = (props) => {
+  return(
+    <section className='companyName'>
+      {props.data.map(job => <Job {...job}/>)}
+    </section>
+  )
+}
+      const HistoryContainer = (props) => {
+        return(
+            <section className='historyContainer'>
+                <JobList data = {props.data}></JobList>
+            </section>
     )
+}
+
+class FormContainer extends React.Component {
+    render(){
+        return(
+            <section className='formContainer'>
+                <Form onSubmit={this.props.onSubmit} />
+            </section>
+        )
+    }
+}
+
+
+class Container extends React.Component {
+
+  addNewCompany = (newCompanyData) => {
+      this.setState(prevState => ({ 
+                                                            dataSet: prevState.dataSet.concat(newCompanyData)
+                                }
+                               )
+                 );
   }
-  const JobList = (props) => {
-    return(
-      <section className='companyName'>
-        {props.data.map(job => <Job {...job}/>)}
-      </section>
-    )
-  }
-        const HistoryContainer = (props) => {
-          return(
-              <section className='historyContainer'>
-                  <JobList data = {props.data}></JobList>
-              </section>
-      )
-  }
-        const FormContainer = (props) => {
-          return(
-              <section className='formContainer'>
-                  <Form />
-              </section>
-      )
-  }
-        const Container = (props) => {
+          state = {
+      dataSet: [
+        {CompanyName: "Google",},
+        {CompanyName: "Netflix"}
+      ]
+  };
+  render(){
     return(
     <section>
       <section className='mainContainer'>
-          <FormContainer />
-        <HistoryContainer data = {props.data} />
+        <FormContainer onSubmit={this.addNewCompany} />
+        <HistoryContainer data = {this.state.dataSet} />
       </section>
     </section>
     )
   }
+}
 
-  let dataSet = [
-    {CompanyName: "Google",},
-    {CompanyName: "Netflix"}
-  ]
-  
-  
-  
-  const App = (props) => {
-    return(
-        <Container data = {props.data}/>
-    )
-  }
-  ReactDOM.render(<App data={dataSet} />,mountNode)
+
+ReactDOM.render(<Container  />,mountNode)
