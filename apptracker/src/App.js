72c1bddc8b4141372a8454a13d64e09ee90f8cd6
frankbenevento <fb.mentoslabs@gmail.com>
2018-09-18 12:17:34 -0700

    added styling and fixed the row issue

--- apptracker/src/App.js
+++ apptracker/src/App.js
@@ -1,44 +1,47 @@
-import React from 'react';
-import './static/css/index.css';
-import HistoryContainer from './components/historyContainer'
-import FormContainer from './components/formContainer'
+  import React from 'react';
+  import './static/css/index.css';
+  import HistoryContainer from './components/historyContainer'
+  import FormContainer from './components/formContainer'
 
 
 
+  class Container extends React.Component {
+
+    addNewCompany = (newCompanyData) => {
+      this.setState(prevState => ({ 
+                    dataSet: prevState.dataSet.concat(newCompanyData)
+                    }));
+    }
+    state = {
+            dataSet: [
+                        {
+                          CompanyName: "Google",
+                          DateContacted:"1/1/2018",
+                          JobSource:"Monster",
+                          MethodOfContact:"Emailed",
+                          FollowUpDate:"2/2/2018"
+                        },
+                        {
+                          CompanyName: "Netflix",
+                          DateContacted:"2/2/2018",
+                          JobSource:"Monster",
+                          MethodOfContact:"Emailed",
+                          FollowUpDate:"2/2/2018"
+
+                        }
+                      ]
+            };
+    render(){
+            return(
+                  <section>
+                      <section className='mainContainer'>
+                          <FormContainer onSubmit={this.addNewCompany} />
+                          <HistoryContainer data = {this.state.dataSet} />
+                      </section>
+                  </section>
+                  )
+            }
+  }
 
 
-
-
-
-
-
-class Container extends React.Component {
-
-addNewCompany = (newCompanyData) => {
-   console.log(newCompanyData)
-    this.setState(prevState => ({ 
-                                dataSet: prevState.dataSet.concat(newCompanyData)
-                                }
-                               )
-               );
-}
-        state = {
-    dataSet: [
-      {CompanyName: "Google",DateContacted:"1/1/2018"},
-      {CompanyName: "Netflix",DateContacted:"2/2/2018"}
-    ]
-};
-render(){
-  return(
-  <section>
-    <section className='mainContainer'>
-      <FormContainer onSubmit={this.addNewCompany} />
-      <HistoryContainer data = {this.state.dataSet} />
-    </section>
-  </section>
-  )
-}
-}
-
-
-export default Container;
+  export default Container;
--- apptracker/src/components/job.js
+++ apptracker/src/components/job.js
@@ -3,21 +3,21 @@ import React from 'react';
 const Job = (props) => {
     return(
 
-      <section className='job-container'>
-        <div className='job-header'>
-            <div>
+      <section>
+        <div className='job-row'>
+            <div className='job-text'>
                 <p>{props.CompanyName}</p>
             </div>
-            <div>
+            <div className='job-text'>
                 <p>{props.DateContacted}</p>
             </div>
-            <div>
+            <div className='job-text'>
                 <p>{props.JobSource}</p>
             </div>
-            <div>
+            <div className='job-text'>
                 <p>{props.MethodOfContact}</p>
             </div>
-            <div>
+            <div className='job-text'>
                 <p>{props.FollowUpDate}</p>
             </div>
         </div>
