3161ab89610e70eab486096bda1b5a3122b84bc9
frankbenevento <fb.mentoslabs@gmail.com>
2018-09-21 07:30:42 -0700

    testing deployment

--- apptracker/src/components/jobHeader.js
+++ apptracker/src/components/jobHeader.js
@@ -9,7 +9,7 @@ const JobHeader = (props) => {
                 <p>Company Name</p>
             </div>
             <div className='job-category'>
-                <p>Date Contacted</p>
+                <p>Date Contactedssss</p>
             </div>
             <div className='job-category'>
                 <p>Job Source</p>
